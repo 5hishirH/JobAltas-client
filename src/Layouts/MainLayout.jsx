825a1dc52a068a3162bbe7fe5c2c0eb33a59dbef
Shishir Halder <s.halder0108@gmail.com>
2023-11-09 17:07:10 +0600

    feat: add footer

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../Components/Header/Navbar'
 import { Outlet } from 'react-router-dom'
+import Footer from '../Components/Footer'
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className='min-h-screen relative'>
         <Navbar></Navbar>
         <Outlet></Outlet>
+        <div className='h-64'></div>
+        <Footer></Footer>
     </div>
   )
 }
